652b676797235124c5f4be954427e1fd715dab27
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-12 09:20:55 +0600

    Added admin route

---
+++ client/src/routes/AdminRoute.jsx
@@ -0,0 +1,13 @@
+import useRole from "./../hooks/useRole";
+import LoadingSpinner from "./../components/Shared/LoadingSpinner";
+import { Navigate } from "react-router-dom";
+const AdminRoute = ({ children }) => {
+  const [role, isLoading] = useRole();
+  if (isLoading) return <LoadingSpinner />;
+
+  if (role === "admin") return children;
+
+  return <Navigate to="/dashboard" replace="true" />;
+};
+
+export default AdminRoute;
