f7b633c2e597e836beffbea0c0678fd4100b6a0d
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-15 14:09:05 +0600

    Navigate user and seller to their inventory and order list page

--- client/src/pages/Dashboard/Common/Statistics.jsx
+++ client/src/pages/Dashboard/Common/Statistics.jsx
@@ -1,6 +1,11 @@
-import { Helmet } from 'react-helmet-async'
-import AdminStatistics from '../../../components/Dashboard/Statistics/AdminStatistics'
+import { Helmet } from "react-helmet-async";
+import AdminStatistics from "../../../components/Dashboard/Statistics/AdminStatistics";
+import { Navigate } from "react-router-dom";
+import useRole from "../../../hooks/useRole";
 const Statistics = () => {
+  const [role] = useRole();
+  if (role === "customer") return <Navigate to="/dashboard/my-orders" />;
+  if (role === "seller") return <Navigate to="/dashboard/my-inventory" />;
   return (
     <div>
       <Helmet>
@@ -8,7 +13,7 @@ const Statistics = () => {
       </Helmet>
       <AdminStatistics />
     </div>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
